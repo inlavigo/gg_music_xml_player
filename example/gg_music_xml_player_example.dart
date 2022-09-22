c7d466d2671f96848a396e30aad67e21c7bd631f
Gabriel Gatzsche <gabriel.gatzsche@inlavigo.com>
2022-09-22 09:46:35 +0200

    Initial commit

---
+++ example/gg_music_xml_player_example.dart
@@ -0,0 +1,6 @@
+import 'package:gg_music_xml_player/gg_music_xml_player.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/gg_music_xml_player.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library gg_music_xml_player;
+
+export 'src/gg_music_xml_player_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
