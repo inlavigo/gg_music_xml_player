ef7c4af0049c8183504439e1fb97abbde682828c
Gabriel Gatzsche <gabriel.gatzsche@grace.cloud>
2022-11-03 12:04:57 +0100

    Add sample xml without bass track

--- lib/gg_music_xml_player.dart
+++ lib/gg_music_xml_player.dart
@@ -12,6 +12,7 @@ export 'package:gg_timeline/gg_timeline.dart';
 // Export own stuff
 export 'src/player/gg_music_xml_player_player.dart';
 export 'src/sample_xml/example_music_xml_doc/example_music_xml_doc.dart';
+export 'src/sample_xml/example_music_xml_doc/example_music_xml_without_bass_track.dart';
 export 'src/timelines/gg_chord_timeline.dart';
 export 'src/timelines/gg_key_timeline.dart';
 export 'src/timelines/gg_note_timeline.dart';
