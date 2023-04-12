6628af0ed7963d51129acbe7d0aaab6077b4ba55
Gabriel Gatzsche <gabriel.gatzsche@grace.cloud>
2023-04-12 14:47:05 +0200

    Add example music xml having a wide range

--- lib/gg_music_xml_player.dart
+++ lib/gg_music_xml_player.dart
@@ -3,22 +3,21 @@
 /// More dartdocs go here.
 library gg_music_xml_player;
 
-// Export music xml lib
-export 'package:music_xml/music_xml.dart';
-
 // Export timeline
 export 'package:gg_timeline/gg_timeline.dart';
+// Export music xml lib
+export 'package:music_xml/music_xml.dart';
 
 // Export own stuff
 export 'src/sample_xml/bibabutzemann/with_bass/gg_example_music_xml_bibabutzemann_with_bass.dart';
 export 'src/sample_xml/bibabutzemann/without_bass/gg_example_music_xml_bibabutzemann_without_bass.dart';
 export 'src/sample_xml/empty/gg_example_music_xml_empty.dart';
 export 'src/sample_xml/key_changes/gg_example_music_xml_key_changes.dart';
+export 'src/sample_xml/pauses_and_notes/gg_example_music_xml_pauses_and_notes.dart';
 export 'src/sample_xml/tempo_changes/gg_example_music_xml_tempo_changes.dart';
 export 'src/sample_xml/tied_note/gg_example_music_xml_tied_note.dart';
+export 'src/sample_xml/wide_range/gg_example_music_xml_wide_range.dart';
 export 'src/sample_xml/without_chord/gg_example_music_xml_without_chord.dart';
-export 'src/sample_xml/pauses_and_notes/gg_example_music_xml_pauses_and_notes.dart';
-
 export 'src/timelines/gg_chord_timeline.dart';
 export 'src/timelines/gg_key_timeline.dart';
 export 'src/timelines/gg_note_timeline.dart';
